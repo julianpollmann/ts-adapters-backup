989768a5654779915b3fa450b80db1ef6118a769
julianpollmann <julian.pollmann@gmx.de>
2023-09-20 17:55:39 +0200

    Finetuning Seq2Seq

--- src/models/finetuning_seq2seq.py
+++ src/models/finetuning_seq2seq.py
@@ -1,26 +1,12 @@
 import argparse
-from os import path, listdir
 
-import numpy as np
 from datasets import load_dataset
-from easse.fkgl import corpus_fkgl
-from easse.sari import corpus_sari
-from evaluate import load
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, DataCollatorForSeq2Seq, Seq2SeqTrainingArguments, \
-    Seq2SeqTrainer, logging
-from transformers.trainer_utils import get_last_checkpoint
-
-logging.set_verbosity_info()
-logger = logging.get_logger("transformers")
+    Seq2SeqTrainer
 
 
 def main(data_args: argparse.Namespace):
-    dataset = load_dataset(data_args.dataset)
-
-    if data_args.sample:
-        dataset = dataset.shuffle(seed=42)
-        dataset["train"] = dataset["train"].select(range(data_args.sample))
-        dataset["validation"] = dataset["validation"].select(range(round(data_args.sample * 0.1)))
+    dataset = load_dataset(data_args.dataset, name=data_args.language)
 
     tokenizer = AutoTokenizer.from_pretrained(data_args.checkpoint)
     model = AutoModelForSeq2SeqLM.from_pretrained(data_args.checkpoint)
@@ -47,32 +33,9 @@ def main(data_args: argparse.Namespace):
     dataset = dataset.map(preprocess_function, remove_columns=column_names, batched=True)
     dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "labels"])
 
-    bleu = load("bleu")
-    ter = load("ter")
-
-    def compute_metrics(eval_pred):
-        predictions, labels, inputs = eval_pred
-        decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
-        labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
-        inputs = np.where(inputs != -100, inputs, tokenizer.pad_token_id)
-        decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
-        decoded_inputs = tokenizer.batch_decode(inputs, skip_special_tokens=True)
-
-        bleu_score = bleu.compute(predictions=decoded_preds, references=decoded_labels)
-        ter_score = ter.compute(predictions=decoded_preds, references=decoded_labels, case_sensitive=True)
-        sari_score = corpus_sari(orig_sents=decoded_inputs, sys_sents=decoded_preds, refs_sents=[decoded_labels])
-        fkgl_score = corpus_fkgl(sentences=decoded_preds)
-
-        return {
-            "Bleu": bleu_score.get("bleu"),
-            "TER": ter_score.get("score"),
-            "SARI": sari_score,
-            "FKGL": fkgl_score
-        }
-
     training_args = Seq2SeqTrainingArguments(
         output_dir=data_args.output_dir,
-        remove_unused_columns=False,
+        remove_unused_columns=True,
         evaluation_strategy="steps",
         logging_strategy="steps",
         per_device_train_batch_size=data_args.batch_size,
@@ -81,8 +44,8 @@ def main(data_args: argparse.Namespace):
         max_steps=data_args.steps,
         save_total_limit=3,
         optim="adamw_torch",
-        predict_with_generate=True,
-        include_inputs_for_metrics=True,
+        #predict_with_generate=True,
+        #include_inputs_for_metrics=True,
         fp16=True,
         eval_steps=100,
         logging_steps=100,
@@ -96,32 +59,15 @@ def main(data_args: argparse.Namespace):
         train_dataset=dataset["train"],
         eval_dataset=dataset["validation"],
         tokenizer=tokenizer,
-        # compute_metrics=compute_metrics
     )
 
-    last_checkpoint = None
-    if path.isdir(training_args.output_dir):
-        last_checkpoint = get_last_checkpoint(training_args.output_dir)
-        if last_checkpoint is None and len(listdir(training_args.output_dir)) > 0:
-            raise ValueError(
-                f"Output directory ({training_args.output_dir}) already exists and is not empty. "
-                "Use --overwrite_output_dir to overcome."
-            )
-        elif last_checkpoint is not None and training_args.resume_from_checkpoint is None:
-            logger.info(
-                f"Checkpoint detected, resuming training at {last_checkpoint}. To avoid this behavior, change "
-                "the `--output_dir` or add `--overwrite_output_dir` to train from scratch."
-            )
-
-    train_result = trainer.train(resume_from_checkpoint=last_checkpoint)
+    train_result = trainer.train()
     trainer.save_model()
 
     metrics = train_result.metrics
-
-    max_train_samples = (
-        data_args.max_train_samples if data_args.max_train_samples is not None else len(dataset["train"])
-    )
-    metrics["train_samples"] = min(max_train_samples, len(dataset["train"]))
+    metrics["train_samples"] = len(dataset["train"])
+    metrics["total_flos"] = trainer.state.total_flos
+    metrics["model_params"] = model.num_parameters()
 
     trainer.log_metrics(split="train", metrics=metrics)
     trainer.save_metrics(split="train", metrics=metrics)
@@ -133,15 +79,15 @@ if __name__ == "__main__":
                                      description="Finetuning Text Simplification")
     parser.add_argument("output_dir", type=str, help="Output dir to store trained checkpoints")
     parser.add_argument("dataset", type=str, help="Name of the dataset in Huggingface Datasets format")
-    parser.add_argument("--checkpoint", type=str, nargs=1, default="facebook/bart-base",
+    parser.add_argument("--checkpoint", type=str, default="facebook/bart-large",
                         help="Model Checkpoint, can be either a pretrained model or a own checkpoint. Defaults to bart-base.")
     parser.add_argument("--steps", type=int, default=400, help="Number of steps to train")
     parser.add_argument("--max_input_length", type=int, default=1024,
                         help="Max input length of tokenized text. Defaults to 1024")
     parser.add_argument("--max_target_length", type=int, default=1024,
                         help="Max target length of tokenized text. Defaults to 1024")
-    parser.add_argument("--batch_size", type=int, default=32, help="Batch size")
-    parser.add_argument("--sample", type=int)
+    parser.add_argument("--batch_size", type=int, default=8, help="Batch size")
+    parser.add_argument("--language", type=str, default=None)
     parser.add_argument("--max_train_samples", type=int)
     parser.add_argument("--learning_rate", type=str, default=2e-5)
 
